54c1135c9f25254e4f0a5fd2bb21ae28e9776ad4
crawlersick <johnbyyang@gmail.com>
2019-03-07 18:24:54 +0800

    ..

--- src/views/chart3/index.vue
+++ src/views/chart3/index.vue
@@ -1,26 +1,57 @@
 <template>
   <div>
-    <b-table
-      id="table-transition-example"
-      :items="items"
-      :fields="fields"
-      :tbody-transition-props="transProps"
-      striped
-      small
-      primary-key="a"
-    />
+    <div>
+      <b-modal id="modaxl" ref="myModalRef" size="xl" title="Large Modal">
+        <div>
+          <b-table
+            id="table-transition-example"
+            :items="items"
+            :fields="fields"
+            :tbody-transition-props="transProps"
+            striped
+            small
+            primary-key="a"
+          />
+        </div>
+      </b-modal>
+    </div>
+    <div>
+      <vepie :data="piechartData1" :settings="piechartSettings1" :events="piechartEvents1"/>
+    </div>
   </div>
 </template>
 
 <script>
+import vepie from 'v-charts/lib/pie.common'
+import BModal from 'bootstrap-vue/es/components/modal/modal'
 import BTable from 'bootstrap-vue/es/components/table/table'
 export default {
-  components: { BTable },
+  components: { vepie, BModal, BTable },
   data() {
+    var self = this
+    this.piechartEvents1 = {
+      click: function(e) {
+        // self.name = e.name
+        console.log(e)
+        console.log(e.data['name'])
+        console.log(e.data['value'])
+        self.$refs.myModalRef.show()
+      }
+    }
+    this.piechartSettings1 = {
+      dataType: 'percent'
+    }
     return {
-      transProps: {
-        // Transition name
-        name: 'flip-list'
+      piechartData1: {
+        columns: ['日期', '访问用户'],
+        rows: [
+          { '日期': '1/1', '访问用户': 1393 },
+          { '日期': '1/2', '访问用户': 3530 },
+          { '日期': '1/3', '访问用户': 2923 },
+          { '日期': '1/4', '访问用户': 1723 },
+          { '日期': '1/5', '访问用户': 3792 },
+          { '日期': '1/6', '访问用户': 4593 }
+        ]
       },
       items: [
         { a: 2, b: 'Two', c: 'Moose' },
@@ -33,6 +64,14 @@ export default {
         { key: 'b', sortable: true },
         { key: 'c', sortable: true }
       ]
+    }
+  },
+  methods: {
+    showModal() {
+      this.$refs.myModalRef.show()
+    },
+    hideModal() {
+      this.$refs.myModalRef.hide()
     }
   }
 }
